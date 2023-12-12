9d508314715e82f769199ce1d68c0fd7106fe53f
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-12 15:32:51 +0100

    Adds keyboard wrapper

---
+++ keyboardWrapper.py
@@ -0,0 +1,30 @@
+import pyautogui
+
+class Keyboard():
+    def writeText(self):
+        '''
+        '''
+        pass
+
+    def pressAndUnpressKey(self):
+        '''
+        '''
+        pass
+
+    def pressKey(self):
+        '''
+        '''
+        pass
+    
+    def unpressKey(self):
+        '''
+        '''
+        pass
+
+    def pressKeyCombination(self):
+        '''
+        '''
+        pass
+
+if __name__ == '__main__':
+    pass
