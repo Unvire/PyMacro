a7342b0593eea8247655164547b773fe973869ad
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-12 15:39:06 +0100

    Adds press key once

--- keyboardWrapper.py
+++ keyboardWrapper.py
@@ -1,6 +1,31 @@
 import pyautogui
 
 class Keyboard():
+    '''
+    KEYS:
+    ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+    ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+    'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+    'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+    'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+    'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+    'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+    'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+    'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+    'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+    'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+    'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+    'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+    'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+    'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+    'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+    'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+    'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+    'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+    'command', 'option', 'optionleft', 'optionright']
+    '''
     def writeString(self, text, interval=0):
         '''
         Converts given text to keypresses, which result in emulating of writing.
@@ -9,17 +34,18 @@ class Keyboard():
         '''
         pyautogui.write(text, interval)
 
-    def pressAndUnpressKey(self):
+    def pressOnce(self, key):
         '''
+        Presses and unpresses key
         '''
-        pass
+        pyautogui.press(key)
 
-    def pressKey(self):
+    def pressAndHold(self):
         '''
         '''
         pass
     
-    def unpressKey(self):
+    def unpress(self):
         '''
         '''
         pass
@@ -31,4 +57,5 @@ class Keyboard():
 
 if __name__ == '__main__':
     kb = Keyboard()
-    kb.writeString('Test\n')
+    kb.writeString('Test')
+    kb.pressOnce('a')
