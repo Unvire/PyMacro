55baf5f363483f6fc196ff5edf00f09b5aaee898
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-13 15:38:07 +0100

    Adds another test to check iteration possibility

--- loadMacro.py
+++ loadMacro.py
@@ -1,17 +1,30 @@
 import json
+from timeit import default_timer as timer
 import cursorWrapper
 
-macro = {1:{
-        'name': 'Move mouse',
-        'enabled': True,
-        'function': cursorWrapper.moveToCoords,
-        'parameters':{
-            'coords': [300, 400]}
-            }
-        }
-
 if __name__ == '__main__':
-    print(macro[1]['function'])
-    print(macro[1]['parameters'])
-
-    macro[1]['function'](**macro[1]['parameters'])
+    ## example task execution
+    macro = {
+        0:{                                     # id: int
+        'name': 'Move mouse',                   # task name: str
+        'enabled': True,                        # enable: bool
+        'function': cursorWrapper.moveToCoords, # function name from module
+        'parameters':{                          # parameters dictionary; must be the same as in the calling function, can be less
+            'coords': [300, 400],
+            'interval': 3}
+            },
+        1:{                                     
+        'name': 'Move mouse',                   
+        'enabled': True,                        
+        'function': cursorWrapper.moveToCoords, 
+        'parameters':{                          
+            'coords': [500, 600]
+            },
+        }
+    }
+    for i in range(2):
+        timeStart = timer()
+        if macro[i]['enabled']:
+            macro[i]['function'](**macro[i]['parameters'])
+        timeEnd = timer()
+        print(f'Elapsed time: {timeEnd - timeStart}')
