a891235c1f8bb7c50065eabeaf659e52b15a7124
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-13 08:47:52 +0100

    Adds region processing

--- imageWrapper.py
+++ imageWrapper.py
@@ -1,6 +1,18 @@
 import pyautogui
 
 class AutoImage:
+    def _processCoords(self, coords=(None, None, None, None)) -> (int, int):
+        '''
+        Method for normalizing coords. They are converted into string, checked if they are numeric and converted into int values. 
+        Method returns None for non numeric coordinate, for example '10', 'a', 0, 2 -> 10, None, 0, 2
+            coords= top, left, width, height sequence
+        '''
+        top, left, width, height = coords
+        top = int(top) if str(top).isnumeric() else None
+        left = int(left) if str(left).isnumeric() else None
+        width = int(width) if str(width).isnumeric() else None
+        height = int(height) if str(height).isnumeric() else None
+        return top, left, width, height
 
     def screenshot(self, fileName=None, region=None):
         '''
@@ -8,7 +20,10 @@ class AutoImage:
             fileName - str
             region - left, top, width, height - sequence of 4 ints
         '''
+        region = self._processCoords(coords=region)
         return pyautogui.screenshot(fileName, region)
+    
+
 
 if __name__ == '__main__':
     img = AutoImage()
