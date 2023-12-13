4ec63d48932e0baaaee5acb9d712c311967c60d6
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-13 14:30:50 +0100

    Replaces Image class with functions

--- imageWrapper.py
+++ imageWrapper.py
@@ -1,50 +1,49 @@
 import pyautogui
 
-class AutoImage:
-    def _processCoords(self, coords=(None, None, None, None)) -> (int, int):
-        '''
-        Method for normalizing coords. They are converted into string, checked if they are numeric and converted into int values. 
-        Method returns None for non numeric coordinate, for example '10', 'a', 0, 2 -> 10, None, 0, 2
-            coords= top, left, width, height sequence
-        '''
-        top, left, width, height = coords
-        top = int(top) if str(top).isnumeric() else None
-        left = int(left) if str(left).isnumeric() else None
-        width = int(width) if str(width).isnumeric() else None
-        height = int(height) if str(height).isnumeric() else None
-        return top, left, width, height
 
-    def screenshot(self, fileName=None, region=None):
-        '''
-        Makes a screenshot of requested area. If fileName is passed then it will save it to a file.
-            fileName - str
-            region - left, top, width, height - sequence of 4 ints
-        '''
-        region = self._processCoords(coords=region)
-        return pyautogui.screenshot(fileName, region)
-    
-    def locateImage(self, searchType='any', image=None, region=None, grayscale=False):
-        '''
-        Checks if image is present in searchedImage. Returns sequence (left, top, width, height) for 'any' or generator that yields (left, top, width, height) for all
-            searchType - 'any' - method will stop after first match
-                         'all' - method will find all occurances of image
-            image - filename/path of image to be searched
-            region - left, top, width left - sequence of 4 ints.
-            grayscale - convert image to grayscale in order to iprove search time. Can result in false matches
-        '''
-        locateFunction = {'any':pyautogui.locateOnScreen, 'all':pyautogui.locateAllOnScreen}
-        return locateFunction[searchType](image=image, region=region, grayscale=grayscale)
+def _processCoords(coords=(None, None, None, None)) -> (int, int):
+    '''
+    Method for normalizing coords. They are converted into string, checked if they are numeric and converted into int values. 
+    Method returns None for non numeric coordinate, for example '10', 'a', 0, 2 -> 10, None, 0, 2
+        coords= top, left, width, height sequence
+    '''
+    top, left, width, height = coords
+    top = int(top) if str(top).isnumeric() else None
+    left = int(left) if str(left).isnumeric() else None
+    width = int(width) if str(width).isnumeric() else None
+    height = int(height) if str(height).isnumeric() else None
+    return top, left, width, height
+
+def screenshot(fileName=None, region=None):
+    '''
+    Makes a screenshot of requested area. If fileName is passed then it will save it to a file.
+        fileName - str
+        region - left, top, width, height - sequence of 4 ints
+    '''
+    region = self._processCoords(coords=region)
+    return pyautogui.screenshot(fileName, region)
+
+def locateImage(searchType='any', image=None, region=None, grayscale=False):
+    '''
+    Checks if image is present in searchedImage. Returns sequence (left, top, width, height) for 'any' or generator that yields (left, top, width, height) for all
+        searchType - 'any' - method will stop after first match
+                        'all' - method will find all occurances of image
+        image - filename/path of image to be searched
+        region - left, top, width left - sequence of 4 ints.
+        grayscale - convert image to grayscale in order to iprove search time. Can result in false matches
+    '''
+    locateFunction = {'any':pyautogui.locateOnScreen, 'all':pyautogui.locateAllOnScreen}
+    return locateFunction[searchType](image=image, region=region, grayscale=grayscale)
         
 
     
 
 
 if __name__ == '__main__':
-    img = AutoImage()
-    img.screenshot(fileName='test.png', region=(700, 500, 300, 200))
-    coords1 = img.locateImage(searchType='any', image='test.png', region=(0, 0, 1200, 1000))
+    screenshot(fileName='test.png', region=(700, 500, 300, 200))
+    coords1 = locateImage(searchType='any', image='test.png', region=(0, 0, 1200, 1000))
     print(f'Coords of any matching image: {coords1}')
-    coords = img.locateImage(searchType='all', image='test2.png', region=(0, 0, 1200, 1000))
+    coords = locateImage(searchType='all', image='test2.png', region=(0, 0, 1200, 1000))
     print('Coords of all matching images')
     for c in coords:
         print(c)
