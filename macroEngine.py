df2645bd631961f17520f46198525ad2e4608b09
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-14 10:34:14 +0100

    Adds docstrings

--- macroEngine.py
+++ macroEngine.py
@@ -7,6 +7,12 @@ import imageFunctions
 
 class Task:
     def __init__(self, name='', isEnabled=False, executeFunction=None, parameters=None):
+        '''
+        name: str -> name of the task
+        isEnabled: bool -> disabled task are not executed
+        executeFunction -> reference for function to be executed
+        parameters: dict -> dictionary of parameters, that are passed as keyword arguments to executeFunction 
+        '''
         self.name = name
         self.isEnabled = isEnabled
         self.executeFunction = executeFunction
@@ -17,6 +23,9 @@ class MacroEngine:
         self.taskList = []
 
     def _createTask(self, taskDict=None):
+        '''
+        Converts taskDict to instance of Task class. Reuturns that instance
+        '''
         name = taskDict['name']
         isEnabled = taskDict['enabled']
         packageName, taskName = taskDict['function'].split('.')  
@@ -29,12 +38,15 @@ class MacroEngine:
         
 
     def loadJSON(self, filePath):
+        '''
+        Opens json file, converts data from dictionary to Task instances and appends them to self.taskList
+        '''
         with open(filePath, 'r') as file:
             taskDict = json.load(file)
 
         for taskID in taskDict:
             self._createTask(taskDict=taskDict[taskID])
-            
+
     def saveJSON(self):
         pass
 
