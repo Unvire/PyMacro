741cdaa440828de0cc94d0c8cfc74f5107aab69e
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-14 09:08:37 +0100

    init file for executing tasks engine

---
+++ macroEngine.py
@@ -0,0 +1,28 @@
+import json
+from timeit import default_timer as timer
+
+import keyboardFunctions
+import cursorFunctions
+import imageFunctions
+
+class Task:
+    pass
+
+class MacroEngine:
+    def __init__(self):
+        pass
+
+    def _createTasks(self):
+        pass
+
+    def loadJSON(self):
+        pass
+
+    def saveJSON(self):
+        pass
+
+    def executeTask(self):
+        pass
+
+if __name__ == '__main__':
+    pass
