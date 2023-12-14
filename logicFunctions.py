0b4b754681d65575cad7a33cda52e38ea38aa33c
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-14 13:59:59 +0100

    Adds checkCondition - if statement that can be called from file

--- logicFunctions.py
+++ logicFunctions.py
@@ -1,27 +1,32 @@
 import re
 
-def equal(val1=None, val2=None):
+def checkCondition(evalationFunctionName='equal', vals=[1, 2], resultTrue='2', resultFalse='1'):
+    evalationFunction = globals()[evalationFunctionName]
+    result = evalationFunction(*vals)
+    return resultTrue if result else resultFalse
+
+def equal(val1, val2):
     return val1 == val2
 
-def notEqual(val1=None, val2=None):
+def notEqual(val1, val2):
     return not equal(val1, val2)
 
-def greater(val1=None, val2=None):
+def greater(val1, val2):
     return int(val1) > int(val2)
 
-def greaterEqual(val1=None, val2=None):
+def greaterEqual(val1, val2):
     return int(val1) >= int(val2)
 
-def less(val1=None, val2=None):
+def less(val1, val2):
     return int(val1) < int(val2)
 
-def lessEqual(val1=None, val2=None):
+def lessEqual(val1, val2):
     return int(val1) <= int(val2)
 
-def inRange(val, lowerLimit=None, upperLimit=None):
+def inRange(val, lowerLimit, upperLimit):
     return int(lowerLimit) <= int(val) <= int(upperLimit)
 
-def notInRange(val, lowerLimit=None, upperLimit=None):
+def notInRange(val, lowerLimit, upperLimit):
     return not inRange(val, lowerLimit, upperLimit)
 
 def regex(string='', pattern='$'):
@@ -37,3 +42,4 @@ if __name__ == '__main__':
     print(inRange(1, 0, 5))
     print(notInRange(-1, 0, 5))
     print(regex(string='aaa', pattern='a*$'), regex(string='aaa', pattern='abc$'))
+    print(checkCondition(evalationFunctionName='regex', vals=['aaa', 'a*$'], resultTrue=2, resultFalse=-1))
