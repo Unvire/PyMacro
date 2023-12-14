68cb9f0cb821eb8ddaf3df136df9a7792a4177c7
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-14 15:21:16 +0100

    init commit for clipboard functions

---
+++ clipboardFunctions.py
@@ -0,0 +1,14 @@
+import pyautogui
+import pyperclip
+
+def copyTextToClipboard(text=''):
+    pass
+
+def copySelectedTextToClipboard():
+    pass
+
+def getFromClipboard():
+    pass
+
+if __name__ == '__main__':
+    pass
