cc340966353a41a50dca7ccdb3dc0bb6e23cd9dd
Unvire <krzysztof.balcerzak6@gmail.com>
2023-12-18 12:28:42 +0100

    init commit

---
+++ pyMacroMain.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+import macroEngine
+
+class pyMacro(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        ## variables
+
+        ## frames
+
+        ## widgets
+
+        ## position
+
+if __name__ == '__main__':
+    app = pyMacro()
+    app.mainloop()
